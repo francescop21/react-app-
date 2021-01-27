55a125405aaef5d0547d1981dfd8b777b1cb8ad7
Francesco Pagano <66972159+francescop21@users.noreply.github.com>
2021-01-27 17:55:32 -0300

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './Componentes/Navbar/index.jsx';
+
+const App = () => {
+  return (
+    <>
+    <NavbarComponent/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Componentes/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavbarComponent = () => {
+    return (
+        <>
+        <nav>
+            Dodo 
+            <a href='https://www.google.com'>Prueba 1</a>
+            <a href='https://www.yahoo.com'>Prueba 2</a>
+        </nav> 
+        </>
+    );
+}
+
+export default NavbarComponent;
